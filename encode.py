546fe2b56c839214ec377e05f001b57ff6d0e4fd
JS <skyjune0824@gmail.com>
2024-10-18 17:31:58 -0400

    first commit

---
+++ encode.py
@@ -0,0 +1,165 @@
+from preprocess.file_manage import save_dict, load_dict, load_json, reset_files, printPkl, splitPkl, clearPklTemp, load_pkls
+from config import PKL_FOLDER, JSON_FOLDER
+from multiprocessing import Pool, Lock, Manager
+from tqdm import tqdm
+import time
+
+import os
+import torch
+os.environ["OMP_NUM_THREADS"] = "16"
+os.environ["MKL_NUM_THREADS"] = "16"
+torch.set_num_threads(16)
+
+
+def dur2vec(duration):
+    return (
+        [1, 0, 0, 0, 0, 0] if duration == -1 else
+        [0, 1, 0, 0, 0, 0] if 0 <= duration <= 500 else
+        [0, 0, 1, 0, 0, 0] if duration <= 1000 else
+        [0, 0, 0, 1, 0, 0] if duration <= 2000 else
+        [0, 0, 0, 0, 1, 0] if duration <= 5000 else
+        [0, 0, 0, 0, 0, 1] if duration >= 5001 else
+        [0, 0, 0, 0, 0, 0]
+    )
+
+
+def age2vec(ages):
+    def min_age_bucket(min_age):
+        return (
+            [1, 0, 0, 0, 0, 0] if min_age == -1 else
+            [0, 1, 0, 0, 0, 0] if 0 <= min_age <= 17 else
+            [0, 0, 1, 0, 0, 0] if min_age == 18 else
+            [0, 0, 0, 1, 0, 0] if 19 <= min_age <= 39 else
+            [0, 0, 0, 0, 1, 0] if 40 <= min_age <= 59 else
+            [0, 0, 0, 0, 0, 1] if min_age >= 60 else
+            [0, 0, 0, 0, 0, 0]
+        )
+
+    def max_age_bucket(max_age):
+        return (
+            [1, 0, 0, 0, 0, 0] if max_age == -1 else
+            [0, 1, 0, 0, 0, 0] if 0 <= max_age <= 18 else
+            [0, 0, 1, 0, 0, 0] if 19 <= max_age <= 39 else
+            [0, 0, 0, 1, 0, 0] if 40 <= max_age <= 59 else
+            [0, 0, 0, 0, 1, 0] if 60 <= max_age <= 79 else
+            [0, 0, 0, 0, 0, 1] if max_age >= 80 else
+            [0, 0, 0, 0, 0, 0]
+        )
+
+    def age_span_bucket(age_span):
+        return (
+            [1, 0, 0, 0, 0, 0] if age_span == -1 else
+            [0, 1, 0, 0, 0, 0] if 0 <= age_span <= 19 else
+            [0, 0, 1, 0, 0, 0] if 20 <= age_span <= 39 else
+            [0, 0, 0, 1, 0, 0] if 40 <= age_span <= 59 else
+            [0, 0, 0, 0, 1, 0] if 60 <= age_span <= 79 else
+            [0, 0, 0, 0, 0, 1] if age_span >= 80 else
+            [0, 0, 0, 0, 0, 0]
+        )
+
+    return min_age_bucket(ages[0]), max_age_bucket(ages[1]), age_span_bucket(ages[2])
+
+
+def match2vec(element, source_file):
+    ls = load_json(source_file)
+    ls = [s for s in ls if s not in ["N/A"]]
+
+    return [1 if s in element else 0 for s in ls]
+
+
+lock = None
+
+
+def init(l):
+    global lock
+    lock = l
+
+
+def process_chunk(split_file, index):
+    split_dict = load_dict(split_file)
+
+    file_names = [f"cities/city_{index}.pkl",
+                  f"states/state_{index}.pkl", f"countries/country_{index}.pkl"]
+    file_paths = [f"{PKL_FOLDER}/bin/{name}" for name in file_names]
+
+    with lock:
+        reset_files(file_paths)
+
+    duration_vecs = {}
+    min_age_vecs = {}
+    max_age_vecs = {}
+    age_span_vecs = {}
+    gender_vecs = {}
+    phase_vecs = {}
+    reason_vecs = {}
+    city_vecs = {}
+    state_vecs = {}
+    country_vecs = {}
+
+    for nctid, data in split_dict.items():
+        duration_vecs[nctid] = dur2vec(data[0][2])
+        min_age_vecs[nctid], max_age_vecs[nctid], age_span_vecs[nctid] = age2vec(
+            data[1])
+        gender_vecs[nctid] = match2vec(data[3], f"{JSON_FOLDER}/gender.json")
+        phase_vecs[nctid] = match2vec(data[4], f"{JSON_FOLDER}/phase.json")
+        reason_vecs[nctid] = match2vec(data[2], f"{JSON_FOLDER}/reason.json")
+        city_vecs[nctid] = match2vec(data[10], f"{JSON_FOLDER}/cities.json")
+        state_vecs[nctid] = match2vec(data[9], f"{JSON_FOLDER}/states.json")
+        country_vecs[nctid] = match2vec(
+            data[8], f"{JSON_FOLDER}/countries.json")
+
+    with lock:
+        save_dict(duration_vecs, f"{PKL_FOLDER}/bin/duration.pkl", True)
+        save_dict(min_age_vecs, f"{PKL_FOLDER}/bin/min_age.pkl", True)
+        save_dict(max_age_vecs, f"{PKL_FOLDER}/bin/max_age.pkl", True)
+        save_dict(age_span_vecs, f"{PKL_FOLDER}/bin/age_span.pkl", True)
+        save_dict(gender_vecs, f"{PKL_FOLDER}/bin/gender.pkl", True)
+        save_dict(phase_vecs, f"{PKL_FOLDER}/bin/phase.pkl", True)
+        save_dict(reason_vecs, f"{PKL_FOLDER}/bin/reason.pkl", True)
+        save_dict(city_vecs, f"{PKL_FOLDER}/bin/cities/city_{index}.pkl", True)
+        save_dict(
+            state_vecs, f"{PKL_FOLDER}/bin/states/state_{index}.pkl", True)
+        save_dict(country_vecs,
+                  f"{PKL_FOLDER}/bin/countries/country_{index}.pkl", True)
+
+
+def process_with(index_and_path):
+    index, path = index_and_path
+
+    return process_chunk(path, index)
+
+
+def encode_features(batch_size=2**13, max_cpu=16):
+    splitPkl(f"{PKL_FOLDER}/full_features.pkl", split_size=batch_size)
+    splited_file_names = load_pkls(f"{PKL_FOLDER}/temp")
+    splited_paths = [
+        f"{PKL_FOLDER}/temp/{name}" for name in splited_file_names]
+
+    file_names = ["gender.pkl", "duration.pkl", "min_age.pkl",
+                  "max_age.pkl", "age_span.pkl", "phase.pkl", "reason.pkl"]
+    file_paths = [f"{PKL_FOLDER}/bin/{name}" for name in file_names]
+
+    reset_files(file_paths)
+
+    indexed_paths = list(enumerate(splited_paths))
+
+    lock = Lock()
+    # Mutex
+
+    with Pool(processes=max_cpu, initializer=init, initargs=(lock,)) as pool:
+        list(tqdm(pool.imap(process_with, indexed_paths), total=len(indexed_paths)))
+
+    clearPklTemp()
+
+
+if __name__ == "__main__":
+
+    st = time.time()
+    encode_features(max_cpu=16)
+    ed = time.time()
+
+    print(f"Total time: {int(ed-st)} sec.")
+
+    # printPkl(f"{PKL_FOLDER}/bin/city.pkl")
+    # printPkl(f"{PKL_FOLDER}/bin/state.pkl")
+    # printPkl(f"{PKL_FOLDER}/bin/country.pkl")
---
+++ file_manage.py
@@ -0,0 +1,88 @@
+import os
+import pickle
+import json
+import pandas as pd
+from config import PKL_FOLDER
+
+
+def reset_files(paths):
+    for file_path in paths:
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+        with open(file_path, 'w') as f:
+            f.write("")
+
+
+def load_pkls(file_dir):
+    names = [name for name in os.listdir(file_dir) if name.endswith(
+        '.pkl') and not name.startswith('full')]
+    return names
+
+
+def save_dict(data, file_path, append=False):
+    if not append or os.path.getsize(file_path) == 0:
+        old_data = data
+    else:
+        with open(file_path, 'rb') as f:
+            old_data = pickle.load(f)
+        old_data.update(data)
+
+    with open(file_path, 'wb') as f:
+        pickle.dump(old_data, f)
+
+
+def load_dict(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+
+
+def save_json(data, file_path):
+    with open(file_path, 'w', encoding='UTF-8') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='UTF-8') as f:
+        return json.load(f)
+
+
+def load_csv(file_path):
+    return pd.read_csv(file_path)
+
+
+def printPkl(file):
+    f = load_dict(file)
+
+    df = pd.DataFrame.from_dict(f, orient="index")
+
+    print(df)
+
+
+def splitPkl(file_path, split_size=2**15):
+    full_dict = load_dict(file_path)
+
+    temp_folder = f"{PKL_FOLDER}/temp"
+
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+
+    for i, chunk_start in enumerate(range(0, len(full_dict), split_size)):
+        chunk_dict = dict(list(full_dict.items())[
+                          chunk_start:chunk_start + split_size])
+
+        chunk_file = os.path.join(temp_folder, f"chunk_{i}.pkl")
+        save_dict(chunk_dict, chunk_file)
+
+
+def clearPklTemp():
+    temp_path = f"{PKL_FOLDER}/temp"
+    names = load_pkls(temp_path)
+
+    for file_name in names:
+
+        if os.path.isfile(f"{temp_path}/{file_name}"):
+            os.remove(f"{temp_path}/{file_name}")
+
+    os.removedirs(temp_path)
